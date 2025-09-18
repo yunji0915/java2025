eaf8132b543bf482cbc59e75d62d73e628fabbc6
yunji0915 <jackie9288@naver.com>
2025-09-18 14:15:18 +0900

    Create p167_4.java

---
+++ 0918/report/p167_4.java
@@ -0,0 +1,12 @@
+public class P167_4 {
+	public static void main(String[] args) {
+		int n [][] = {{1,2,3}, {1,2}, {1}, {1,2,3}, {1,2,3,4}};
+		
+		for(int i=0;i<n.length;i++) {
+			for(int j=0;j<n[i].length;j++) {
+				System.out.print(n[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
