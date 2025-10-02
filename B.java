c3ad1493ee2deb82474909d72ece25e13044c6b2
yunji0915 <jackie9288@naver.com>
2025-10-02 16:09:24 +0900

    Add files via upload

---
+++ B.java
@@ -0,0 +1,10 @@
+package P;
+
+public class B extends A {
+	void set() {
+		pub = 1;
+		pro = 2;
+		def = 3;
+		pri = 4;
+	}
+}
---
+++ C.java
@@ -0,0 +1,12 @@
+package Q;
+import P.A;
+
+public class C {
+	public static void main(String[] args) {
+		C a1 = new C();
+		a1.pub = 1;
+		a1.pro = 2;
+		a1.def = 3;
+		a1.pri = 4;
+	}
+}
