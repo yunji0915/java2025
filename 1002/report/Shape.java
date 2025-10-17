2c9c916fe9b0cedd7223e42f48bde6b0b4ea6713
yunji0915 <jackie9288@naver.com>
2025-10-17 22:23:57 +0900

    Add abstract Shape class with next element support

---
+++ 1002/report/Shape.java
@@ -0,0 +1,9 @@
+package report;
+
+public abstract class Shape {
+	private Shape next;
+	public Shape() { next = null; }
+	public void setNext(Shape obj) { next = obj; }
+	public Shape getNext() { return next; }
+	public abstract void draw();
+}
