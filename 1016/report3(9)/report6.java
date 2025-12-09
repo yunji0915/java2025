404415ff9acc2d4fcc6f90dc2c5ef2724ec44ac1
yunji0915 <jackie9288@naver.com>
2025-12-10 07:54:36 +0900

    Add GUI for random JLabel placement

--- 1016/report3(9)/report6.java
+++ 1016/report3(9)/report6.java
@@ -1,1 +1,32 @@
-report
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+public class Exam_6 {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Random JLabel Placement");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(300, 300);
+		frame.setLayout(null);
+		
+		Random rand = new Random();
+		
+		for(int i=0; i<20; i++) {
+			JLabel label = new JLabel();
+			label.setSize(10,10);
+			label.setOpaque(true);
+			
+			int x = 10 + rand.nextInt(241);
+			int y = 10 + rand.nextInt(241);
+			label.setLocation(x,y);
+			
+			Color randomColor = new Color(rand.nextInt(256), rand.nextInt(256), rand.nextInt(256));
+			label.setBackground(randomColor);
+			
+			frame.add(label);
+		}
+		frame.setVisible(true);
+	}
+}
