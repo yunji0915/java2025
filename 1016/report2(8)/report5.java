fe3899d4a40437299701dd3943345d1894a6e763
yunji0915 <jackie9288@naver.com>
2025-12-10 07:59:55 +0900

    Update report5.java

--- 1016/report2(8)/report5.java
+++ 1016/report2(8)/report5.java
@@ -1,1 +1,31 @@
-d
+package Chapter_10;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+
+public class Exam_5 {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("클릭 연습");
+		frame.setSize(400,400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(null);
+		
+		JLabel label = new JLabel("C");
+		label.setSize(20,20);
+		label.setLocation(100,100);
+		frame.add(label);
+		
+		Random rand = new Random();
+		
+		label.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				int x = rand.nextInt(frame.getContentPane().getWidth()-label.getWidth());
+				int y = rand.nextInt(frame.getContentPane().getHeight()-label.getHeight());
+				label.setLocation(x,y);
+			}
+		});
+		frame.setVisible(true);
+	}
+}
