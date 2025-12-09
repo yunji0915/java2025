413a5119cdb098b0cf6e070c263a5c2417181ba5
yunji0915 <jackie9288@naver.com>
2025-12-10 08:06:46 +0900

    Update report01.java

--- 0911/report/1106/report/report01.java
+++ 0911/report/1106/report/report01.java
@@ -1,31 +1,35 @@
-import java.util.Scanner;
-import java.util.Vector;
+package appl;
 
-public class report_01 {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		Vector <Integer> v = new Vector<Integer>();
-		
-		System.out.print("정수 입력(-1이면 입력 끝)>>");
-		while(true) {
-			int input = scanner.nextInt();
-			
-			if(input == -1) { break; }
-			
-			v.add(input);
-		}
-		
-		int min = v.get(0);
-		
-		for(int i=1; i<v.size(); i++) {
-			if(min > v.get(i)) {
-				min = v.get(i);
-			}
-		}
-		
-		System.out.println("제일 작은 수는 " + min);
-		
-		scanner.close();
+class Student{
+	private String name;
+	private int id;
+	
+	public Student(String name, int id) {
+		this.name = name;
+		this.id = id;
 	}
+	public String toString() {
+		return name + " 학생의 학번은 " + id;
+	}
+	
+	public boolean equals(Object obj) {
+		if (obj instanceof Student) {
+			Student other = (Student) obj;
+			return this.name.equals(other.name) && this.id == other.id;//이름이 같으면 같은 학생
+		}
+		return false;
+	}
+}
+
+public class Exam_1 {
+	public static void main(String[] args) {
+		Student a = new Student("황기태", 23);	//id가 23인 황기태 학생
+		Student b = new Student("황기태", 77);	//id가 77인 황기태 학생
+		System.out.println(a);
+		if(a.equals(b))
+			System.out.println("같은 학생입니다.");
+		else
+			System.out.println("다른 학생입니다.");
+	}
+
 }
