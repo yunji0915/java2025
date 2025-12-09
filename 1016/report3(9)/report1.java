d2aaee029178d6fd76c0d956e58d2ad9dabd1066
yunji0915 <jackie9288@naver.com>
2025-12-10 07:52:52 +0900

    Update report1.java

--- 1016/report3(9)/report1.java
+++ 1016/report3(9)/report1.java
@@ -1,1 +1,16 @@
-d
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Exam_1 {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Make a Window using Swing");
+		
+		frame.setSize(400, 150);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container contentPane = frame.getContentPane();
+		contentPane.setBackground(Color.YELLOW);
+		frame.setVisible(true);
+	}
+}
