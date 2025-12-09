8d23a067649d4cc581ca68bf5b9e2198b995f217
yunji0915 <jackie9288@naver.com>
2025-12-10 07:27:19 +0900

    Refactor to use BorderLayout in GUI application

--- 1113/report/report_2.java
+++ 1113/report/report_2.java
@@ -1,90 +1,28 @@
 /*
- * 실습문제 3
- * 실행예시화면을 구성하고, 금액을 입력한 후 "계산" 버튼을 클릭하면 오만원권,
- * 만원권, 천원권, 500원짜리 동전, 100원짜리 동전, 50원짜리 동전, 10원짜리 동전, 1원짜리 동전으로
- * 분할하여 개수를 출력하라. 이때 가장 큰 액수부터 채워라.
+ * 실습문제 2
+ * BorderLayout을 사용하여 실행 결과와 같이 동서남북에 각각 버튼을 배치하고 이들 사이의 수평 수직 간격이
+ * 각각 5픽셀, 7픽셀이 되도록 스윙 응용프로그램을 작성하라. (East, West, North, South, Center)
  */
 
-package Exam;
+package gui;
 
 import javax.swing.*;
-import java.awt.event.*;
+import java.awt.*;
 
-public class Exam_3 {
+public class Exam_2 {
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("화폐 단위 계산기");
-		frame.setSize(350,400);
+		JFrame frame = new JFrame("BorderLayout Example");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLayout(null);
+		BorderLayout layout = new BorderLayout(5,7);
+		frame.setLayout(layout);
 		
-		JLabel label = new JLabel("금액 입력 : ");
-		label.setBounds(30,20,80,30);
-		frame.add(label);
+		frame.add(new JButton("North"), BorderLayout.NORTH);
+		frame.add(new JButton("South"), BorderLayout.SOUTH);
+		frame.add(new JButton("East"), BorderLayout.EAST);
+		frame.add(new JButton("West"), BorderLayout.WEST);
+		frame.add(new JButton("Center"), BorderLayout.CENTER);
 		
-		JTextField textField = new JTextField();
-		textField.setBounds(110,20,150,30);
-		frame.add(textField);
-		
-		JButton calcButton = new JButton("계산");
-		calcButton.setBounds(270,20,60,30);
-		frame.add(calcButton);
-		
-		String[] unitNames = {
-				"오만원권", "만원권", "천원권", "500원", "100원", "50원", "10원", "1원"
-		};
-		int[] unitValues = {
-				50000, 10000, 1000, 500, 100 , 50, 1
-		};
-		JLabel[] resultLabels = new JLabel[unitNames.length];
-		
-		for(int i =0; i<unitNames.length; i++) {
-			JLabel unitLabel = new JLabel(unitNames[i]+":");
-			unitLabel.setBounds(30,70+i*30,80,30);
-			frame.add(unitLabel);
-			
-			resultLabels[i] = new JLabel("0개");
-			resultLabels[i].setBounds(110, 70+i*30, 100, 30);
-			frame.add(resultLabels[i]);
-		}
-		
-		calcButton.addActionListener(e->{
-			try {
-				int amount = Integer.parseInt(textField.getText());
-				for(int i=0; i<unitValues.length; i++) {
-					int count = amount / unitValues[i];
-					amount %=unitValues[i];
-					resultLabels[i].setText(count+"개");
-				}
-			} catch(NumberFormatException ex) {
-				JOptionPane.showMessageDialog(frame, "숫자를 정확이 입력하세요");
-			}
-		});
+		frame.setSize(400, 200);
 		frame.setVisible(true);
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
