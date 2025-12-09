fb7bf48371d4decfd4965d97e128e81436dc662f
yunji0915 <jackie9288@naver.com>
2025-12-10 07:28:27 +0900

    Refactor to create currency calculator application

--- 1113/report/report_3.java
+++ 1113/report/report_3.java
@@ -1,7 +1,8 @@
 /*
- * 실습문제 2
- * 실행 예시와 같이 JComboBox로 빈 콤보박스를 만들고, JTextField 입력 창에 문자열을
- * 입력한 후 <Enter> 키를 입력하면 문자열을 콤보박스의 아이템으로 삽입하라.
+ * 실습문제 3
+ * 실행예시화면을 구성하고, 금액을 입력한 후 "계산" 버튼을 클릭하면 오만원권,
+ * 만원권, 천원권, 500원짜리 동전, 100원짜리 동전, 50원짜리 동전, 10원짜리 동전, 1원짜리 동전으로
+ * 분할하여 개수를 출력하라. 이때 가장 큰 액수부터 채워라.
  */
 
 package Exam;
@@ -9,28 +10,81 @@ package Exam;
 import javax.swing.*;
 import java.awt.event.*;
 
-public class Exam_2 {
+public class Exam_3 {
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("콤보박스 추가");
-		frame.setSize(300,200);
+		JFrame frame = new JFrame("화폐 단위 계산기");
+		frame.setSize(350,400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLayout(null);
 		
-		JComboBox<String> comboBox = new JComboBox<>();
-		comboBox.setBounds(50,30,200,30);
-		frame.add(comboBox);
+		JLabel label = new JLabel("금액 입력 : ");
+		label.setBounds(30,20,80,30);
+		frame.add(label);
 		
 		JTextField textField = new JTextField();
-		textField.setBounds(50,70,200,30);
+		textField.setBounds(110,20,150,30);
 		frame.add(textField);
 		
-		textField.addActionListener(e->{
-			String input = textField.getText().trim();
-			if(!input.isEmpty()) {
-				comboBox.addItem(input);
-				textField.setText("");
+		JButton calcButton = new JButton("계산");
+		calcButton.setBounds(270,20,60,30);
+		frame.add(calcButton);
+		
+		String[] unitNames = {
+				"오만원권", "만원권", "천원권", "500원", "100원", "50원", "10원", "1원"
+		};
+		int[] unitValues = {
+				50000, 10000, 1000, 500, 100 , 50, 1
+		};
+		JLabel[] resultLabels = new JLabel[unitNames.length];
+		
+		for(int i =0; i<unitNames.length; i++) {
+			JLabel unitLabel = new JLabel(unitNames[i]+":");
+			unitLabel.setBounds(30,70+i*30,80,30);
+			frame.add(unitLabel);
+			
+			resultLabels[i] = new JLabel("0개");
+			resultLabels[i].setBounds(110, 70+i*30, 100, 30);
+			frame.add(resultLabels[i]);
+		}
+		
+		calcButton.addActionListener(e->{
+			try {
+				int amount = Integer.parseInt(textField.getText());
+				for(int i=0; i<unitValues.length; i++) {
+					int count = amount / unitValues[i];
+					amount %=unitValues[i];
+					resultLabels[i].setText(count+"개");
+				}
+			} catch(NumberFormatException ex) {
+				JOptionPane.showMessageDialog(frame, "숫자를 정확이 입력하세요");
 			}
 		});
 		frame.setVisible(true);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
