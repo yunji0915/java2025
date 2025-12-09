75b3c373cea1a02a14d6937ce367be7e224cbc0d
yunji0915 <jackie9288@naver.com>
2025-12-10 07:53:19 +0900

    Implement JFrame with BorderLayout example

--- 1016/report3(9)/report2.java
+++ 1016/report3(9)/report2.java
@@ -1,1 +1,22 @@
-d
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Exam_2 {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("BorderLayout Example");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		BorderLayout layout = new BorderLayout(5,7);
+		frame.setLayout(layout);
+		
+		frame.add(new JButton("North"), BorderLayout.NORTH);
+		frame.add(new JButton("South"), BorderLayout.SOUTH);
+		frame.add(new JButton("East"), BorderLayout.EAST);
+		frame.add(new JButton("West"), BorderLayout.WEST);
+		frame.add(new JButton("Center"), BorderLayout.CENTER);
+		
+		frame.setSize(400, 200);
+		frame.setVisible(true);
+	}
+}
