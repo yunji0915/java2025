764626e6c537e79a8001c1942829fd8d6b1357c9
yunji0915 <jackie9288@naver.com>
2025-12-10 07:30:05 +0900

    Update report_5.java

--- 1113/report/report_5.java
+++ 1113/report/report_5.java
@@ -1,45 +1,44 @@
-package chaptor_10;
+package Exam;
 
-import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
-import javax.swing.event.*;
+import java.awt.event.*;
 
-class MyFrame extends JFrame{
-	JLabel la = new JLabel("C");
-	
-	public MyFrame() {
-		setTitle("클릭 연습 응용프로그램");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		Container c = getContentPane();
-		c.addMouseListener(new MyMouseAdapter());
-				
-		c.setLayout(null);
-		la.setLocation(100, 100);
-		la.setSize(50, 50);
-		c.add(la);
-		
-		setSize(500, 500);
-		setVisible(true);
-		
-		la.setFocusable(true);
-		la.requestFocus();
-	}
-}
-
-class MyMouseAdapter extends MouseAdapter{
-	public void mouseClicked(MouseEvent e) {
-		Container c = (Container)e.getSource();
-		int x = (int)(Math.random()*300);
-		int y = (int)(Math.random()*300);
-		
-		c.setLocation(x, y);
-	}
-}
-
-public class report_5 {
+public class Exam_5 {
 	public static void main(String[] args) {
-		MyFrame my = new MyFrame();
+		JFrame frame = new JFrame("슬라이더 에제");
+		frame.setSize(300,200);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(null);
+		
+		JLabel valueLabel = new JLabel("현재 값 : 100");
+		valueLabel.setBounds(100,30,100,30);
+		frame.add(valueLabel);
+		
+		JSlider slider = new JSlider(JSlider.HORIZONTAL, 100, 200, 100);
+		slider.setBounds(50,80,200,50);
+		slider.setMajorTickSpacing(20);
+		slider.setMinorTickSpacing(5);
+		slider.setPaintTicks(true);
+		slider.setPaintLabels(true);
+		frame.add(slider);
+		
+		slider.addChangeListener(e->{
+			int value = slider.getValue();
+			valueLabel.setText("현재 값: "+value);
+		});
+		
+		frame.setVisible(true);
+		
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
