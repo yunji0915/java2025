4204c4df753dcf6f67acc49f4bc4b0e6584eb2a0
yunji0915 <jackie9288@naver.com>
2025-12-10 08:04:28 +0900

    Update report_01.java

--- 1016/report/report_01.java
+++ 1016/report/report_01.java
@@ -1,29 +1,44 @@
-package main;
+package generic;
+import java.util.*;
 
-class StudentApp{
-	private String name;
-	private int id = 0;
-	
-	public StudentApp(String name, int id){ this.name = name; this.id = id; }
-	public String toString() { return "학번이 " + id + "인 " + name; }
-	public boolean equals(Object obj) {
-		StudentApp st = (StudentApp)obj;
-		if(name.equals(st.name) && (id == st.id)) {
-			return true;			
-		}
-		else return false;
-	}
-}
-
-
-public class report_01 {
+public class Exam_1 {
 	public static void main(String[] args) {
-		StudentApp a = new StudentApp("황기태", 23);
-		StudentApp b = new StudentApp("황기태", 77);
-		System.out.println(a);
-		if(a.equals(b))
-			System.out.println("같은 학생입니다.");
-		else
-			System.out.println("다른 학생입니다.");
+		Scanner scanner = new Scanner(System.in);
+		Vector<Integer> numbers = new Vector<>();
+		
+		System.out.println("양의 정수를 입력하세요. -1을 입력하면 종료");
+		
+		while(true) {
+			System.out.print("입력 >> ");
+			int input = scanner.nextInt();
+			
+			if(input == -1)
+				break;
+			if(input > 0)
+				numbers.add(input);
+			else
+				System.out.println("양의 정수만 입력하세요");
+		}
+		if(numbers.isEmpty()) {
+			System.out.println("입력된 양의 정수가 없습니다.");
+		} else {
+			int min = numbers.get(0);
+			for(int num : numbers) {
+				if(num < min) {
+					min = num;
+				}
+			}
+			System.out.println("가장 작은 수는 : " + min);
+		}
+		scanner.close();
 	}
 }
+
+
+
+
+
+
+
+
+
