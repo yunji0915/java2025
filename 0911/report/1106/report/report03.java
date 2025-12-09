5e72fb8b5ce59d7cdaadc9b6c10bb188af74b4e1
yunji0915 <jackie9288@naver.com>
2025-12-10 08:07:48 +0900

    Create report03.java

---
+++ 0911/report/1106/report/report03.java
@@ -0,0 +1,14 @@
+package appl;
+
+class Calc{
+	private int x, y;
+	public Calc(int x, int y) { this.x = x; this.y = y;}
+	public int sum() { return x+y;}
+}
+
+public class Exam_3 {
+	public static void main(String[] args) {
+		Calc c = new Calc(10,20);
+		System.out.println(c.sum());
+	}
+}
