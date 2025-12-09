eb4311ef04519ba9b65bdf7e17adc60bc0341b1f
yunji0915 <jackie9288@naver.com>
2025-12-10 07:53:43 +0900

    Implement JFrame with GridLayout and buttons

--- 1016/report3(9)/report3.java
+++ 1016/report3(9)/report3.java
@@ -1,1 +1,18 @@
-d
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Exam_3 {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("GridLayout Example");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new GridLayout(1,10));
+		
+		for(int i=1; i<=10; i++) {
+			frame.add(new JButton(String.valueOf(i)));
+		}
+		frame.setSize(800, 100);
+		frame.setVisible(true);
+	}
+}
